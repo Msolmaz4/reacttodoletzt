283f82b49a298cb7f63d597bf0b60fd3a84031ab
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-02-07 22:49:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [todo, setTodo] = useState("");
+
+  const keyD = () => {
+    Math.random();
+  };
+
+ function handleSubmit(e) {
+   e.preventDefault()
+
+   const newTodo ={
+
+    id:new Date().getTime(),
+    text:todo,
+    completed:false
+
+   }
+   setTodos([...todos].concat(newTodo))
+   setTodo('')
+ }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          key={keyD}
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button type="submit">add to</button>
+      </form>
+      {todos.map((e)=><div>{e.text}</div>)}
     </div>
   );
 }
